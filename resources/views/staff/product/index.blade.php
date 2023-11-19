33518a5927471cda72bd72b21a1781352e4cee60
Chayakorn2002 <chayakorn.chi@ku.th>
2023-11-19 20:44:40 +0700

    fix(bug)

--- resources/views/staff/product/index.blade.php
+++ resources/views/staff/product/index.blade.php
@@ -67,8 +67,14 @@
                                 class="flex flex-col group bg-white border border-gray-200 shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition">
 
                                 <div class="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
-                                    <img class="w-full h-full bg-gray-100 absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
-                                        src="{{ asset('/storage/' . $product->images[0]) }}">
+                                    @if (is_array($product->images) && count($product->images) > 0)
+                                        <img class="w-full h-full bg-gray-100 absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+                                            src="{{ asset('/storage/' . $product->images[0]) }}">
+                                    @else
+                                        {{-- Placeholder or default image when there is no product image --}}
+                                        <img class="w-full h-full bg-gray-100 absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+                                            src="{{ asset('path_to_placeholder_image.jpg') }}" alt="Placeholder Image">
+                                    @endif
                                 </div>
                                 <div class="px-5">
                                     <div class="text-sm grid grid-cols-2 my-3">
@@ -117,29 +123,6 @@
                     @endif
 
                 </div>
-                {{-- <div class="mb-8 grid grid-cols-1 grid-rows-4 gap-4 md:grid-cols-2 lg:grid-cols-3 py-10">
-                    @foreach ($products as $product)
-                        <a href="{{ route('order.add-order-item', ['id' => $product->id]) }}" class="flex flex-col group bg-white border border-gray-200 shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition">
-                            <div class="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
-                                <img class="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl" src="../docs/assets/img/500x300/img1.jpg">
-                            </div>
-                            <div class="p-4 md:p-5">
-                                <h3 class="text-xl font-bold text-gray-800 dark:text-white">
-                                    {{ $product->product_name }}
-                                </h3>
-                                <p class="mt-1 text-gray-800 dark:text-gray-400">
-                                    {{ $product->product_description }}
-                                </p>
-                                <div class="flex text-left justify-between ">
-                                    <span class="text-3xl row-start-4 font-bold text-gray-900">{{ number_format($product->product_price, 2) }} ฿</span>
-                                </div>
-                            </div>
-                        </a>
-                    @endforeach
-
-                    @if (count($products) === 0)
-                        <p class="text-gray-500">No Products Available</p>
-                    @endif --}}
             </div>
         </div>
     </section>
